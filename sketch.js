f44f09314d4943019d091e7d9810c40937aaaca8
Shivam Sh <shivam.sharm@icloud.com>
2020-07-29 16:06:41 -0400

    Initialize the Project
    Prepare the foundation for the project to be built on by:
     - Creating a webpage to host the canvas
     - Implementing and testing the very basics of a WEBGL/p5 sketch

---
+++ sketch.js
@@ -0,0 +1,20 @@
+/*
+ * Terra
+ * Shivam Sh | https://github.com/shivam-sh
+ * 
+ * A terrain generator/3D world 
+ * made using WEBGL and p5.js
+ */
+
+// Options
+
+// Global Variables
+
+function setup() {
+	// Create the canvas to enable output to the webpage
+	createCanvas(windowWidth, windowHeight, WEBGL);
+
+}
+
+function draw() {}
+
